6e1033f0e2239765e7d862dbb40f5ae7b170ed75
unknown <gabyhizukuri@gmail.com>
2025-01-29 17:20:01 -0300

    implementando funçao adicionarAmigo()

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,26 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+function adicionarAmigo(){
+    let nome = document.getElementById('amigo').value;
+    if(nome == ''){
+        alert("Por favor, insira um nome.");
+    } else {
+        amigos.push(nome);
+        console.log(amigos) 
+    }
+    limparCampo();
+}
+
+function atualizarAmigos(){
+
+}
+
+function sortearAmigos(){
+
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
